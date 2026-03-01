a1d6fd3ef94dc4e7bb29fa10ce32732df73585b4
overexerted <254196719+overexerted@users.noreply.github.com>
2026-03-01 16:08:35 -0500

    update removal list

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,10 +13,68 @@ set -ouex pipefail
 #dnf5 install -y tmux 
 
 rpm-ostree override remove \
-	firefox \
-	firefox-langpacks \
-	khelpcenter \
-	kcharselect
+    # browsers
+    firefox \
+    firefox-langpacks \
+    plasma-browser-integration \
+    fedora-chromium-config-kde \
+    # kde utils
+    khelpcenter \
+    kcharselect \
+    spectacle \
+    ark \
+    ark-libs \
+    krfb \
+    krfb-libs \
+    kfind \
+    kinfocenter \
+    kdebugsettings \
+    kwalletmanager5 \
+    kde-partitionmanager \
+    kwrite \
+    plasma-welcome \
+    plasma-systemmonitor \
+    plasma-discover \
+    plasma-discover-flatpak \
+    plasma-discover-kns \
+    plasma-discover-libs \
+    plasma-discover-notifier \
+    filelight \
+    # fcitx5
+    fcitx5 \
+    fcitx5-mozc \
+    fcitx5-data \
+    fcitx5-unikey \
+    fcitx5-chewing \
+    fcitx5-sayura \
+    fcitx5-chinese-addons \
+    fcitx5-m17n \
+    fcitx5-chinese-addons-data \
+    fcitx5-lua \
+    fcitx5-hangul \
+    fcitx5-libthai \
+    fcitx5-configtool \
+    fcitx5-gtk \
+    fcitx5-gtk3 \
+    fcitx5-gtk4 \
+    fcitx5-libs \
+    fcitx5-qt \
+    fcitx5-qt-libfcitx5qt6widgets \
+    fcitx5-qt-libfcitx5qtdbus \
+    fcitx5-qt-qt6gui \
+    fcitx5-qt5 \
+    fcitx5-qt6 \
+    kcm-fcitx5 \
+    libime \
+    libime-data \
+    # journald
+    kjournald \
+    kjournald-libs \
+    # akonadi
+    akonadi-server \
+    akonadi-server-mysql \
+    # etc
+    htop
 
 # Use a COPR Example:
 #
